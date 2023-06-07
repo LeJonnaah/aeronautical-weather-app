81ea1d766e05c170007690f242841024b63426d3
Jandueza <jonathanandueza1201@gmail.com>
2023-06-07 16:08:13 -0300

    Styles fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {Component} from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +32,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
+    MatInputModule,
+    MatButtonModule,
     MatFormFieldModule
   ],
   providers: [],
